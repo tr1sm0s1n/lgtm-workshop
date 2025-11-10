f035ac733eefe742aeb3714cd7eaf4071c6c289e
Aneesh R S <aneesh.rs@duk.ac.in>
2025-11-10 15:26:51 +0530

    Feat:Password Strength Checker Added

--- js/math/password-strength-checker/password-checker.js
+++
@@ -1,9 +0,0 @@
-function isStrongPassword(pwd) {
-  return (
-    typeof pwd === "string" &&
-    pwd.length >= 8 &&
-    /[A-Z]/.test(pwd) && // at least one uppercase letter
-    /\d/.test(pwd) // at least one digit
-  );
-}
-module.exports = isStrongPassword;
